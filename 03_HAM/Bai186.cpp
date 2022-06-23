d996aff132371fff039d270054cb8ae6f8d33775
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 20:03:41 +0700

    Create Bai186.cpp

---
+++ 03_HAM/Bai186.cpp
@@ -0,0 +1,6 @@
+Viết chương trình lặp đi lặp lại các công việc sau:
+- Nhập vào một ký tự trên bàn phím.
+- Nếu là chữ thường thì in ra chính nó và chữ HOA tương ứng.
+- Nếu là chữ HOA thì in ra chính nó và chữ thường tường ứng.
+- Nếu là ký số thì in ra chính nó.
+- Nếu là một ký tự điều khiển thì kết thúc chương trình
