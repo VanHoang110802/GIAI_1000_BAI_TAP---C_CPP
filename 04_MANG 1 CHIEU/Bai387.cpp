b1c16537737fecb1074afd48787742e82f23d927
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:33:43 +0700

    Create Bai387.cpp

---
+++ 04_MANG 1 CHIEU/Bai387.cpp
@@ -0,0 +1,62 @@
+Hãy sắp xếp các số dương trong mảng tăng dần,các số âm giảm dần.Vị trí tương đối giữa các số âm và số dương không đổi.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#define Max 100
+void NhapDuLieu(int& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%d", &n);
+		if (n<1||n>Max) {
+			printf("So luong phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+		}
+	} while (n<1||n>Max);
+}
+void NhapMang(int a[], int n) {
+	for (int i = 0; i < n; ++i) {
+		printf("a[%d]= ", i);
+		scanf("%d", &a[i]);
+	}
+}
+void XuatMang(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d  ", a[i]);
+	}
+}
+void HoanVi(int& a, int& b){
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void SapXepSoDuongTangDanVaSoAmGiamDan(int a[], int n){
+	for (int i = 0; i < n - 1; i++){
+		for (int j = i + 1; j < n; j++){
+			if (a[i] > 0 && a[j] > 0 && a[i] > a[j]){
+				HoanVi(a[i], a[j]);
+			}
+			else if (a[i] < 0 && a[j] < 0 && a[i] < a[j])
+			{
+				HoanVi(a[i], a[j]);
+			}
+		}
+	}
+}
+int main(){
+	int n, a[Max];
+hoang:
+	printf("\n");
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	printf("\n");
+	XuatMang(a, n);
+	printf("\n");
+	SapXepSoDuongTangDanVaSoAmGiamDan(a,n);
+	XuatMang(a, n);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
