1ab7cf4d0b2114faa484bd57a04e31f0eb85c8d0
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:52:03 +0700

    Create Bai437.cpp

---
+++ 04_MANG 1 CHIEU/Bai437.cpp
@@ -0,0 +1,8 @@
+Cấp số cộng là một dãy số mà kể từ số hạng thứ hai trở đi thì mỗi số hạng hơn số hạng liền trước nó k đơn vị.
+Ví dụ:
++) 2, 4, 6, 8, 10,… là cấp số cộng có khoảng cách k = 2.
++) 6, 9, 12, 15, …. là cấp số cộng có khoảng cách k = 3.
+Tí đã viết được 4 số tạo thành một cấp số cộng, theo yêu cầu của cô giáo. Nhưng giờ ra chơi, Tèo lại xóa đi một số, rồi viết theo thứ tự ngẫu nhiên 3 số còn lại.
+Bạn hãy giúp Tí khôi phục lại số bị thiếu ban đầu.
+intput: 4 6 8 	-> output: 10
+	10 1 4 		   7
