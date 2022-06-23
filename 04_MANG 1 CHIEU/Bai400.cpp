f31a36e5bafda3b3d0d8eb02b3722a353649f434
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:38:29 +0700

    Create Bai400.cpp

---
+++ 04_MANG 1 CHIEU/Bai400.cpp
@@ -0,0 +1,51 @@
+Xóa các phần tử có chỉ số k trong mảng .
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#define Max 100
+void NhapDuLieu(int& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%d", &n);
+		if (n<1||n>Max) {
+			printf("So luong phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+		}
+	} while (n<1||n>Max);
+}
+void NhapMang(int a[], int n) {
+	for (int i = 0; i < n; ++i) {
+		printf("a[%d]= ", i);
+		scanf("%d", &a[i]);
+	}
+}
+void XuatMang(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d  ", a[i]);
+	}
+}
+void Xoa1PhanTu(int a[], int& n, int ViTriXoa){
+	for (int i = ViTriXoa; i < n; i++){
+		a[i] = a[i + 1];
+	}
+	n--;
+}
+int main(){
+	int n, a[Max],k;
+hoang:
+	printf("\n");
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	printf("\n");
+	XuatMang(a, n);
+	printf("\nNhap k= ");
+	scanf("%d", &k);
+	Xoa1PhanTu(a,n,k);
+	XuatMang(a, n);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
