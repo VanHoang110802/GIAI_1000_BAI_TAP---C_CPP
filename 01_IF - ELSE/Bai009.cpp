25adaae0a2bbb5dcafadd01df7f03772b249b4ae
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 18:15:43 +0700

    Create Bai009.cpp

---
+++ 01_IF - ELSE/Bai009.cpp
@@ -0,0 +1,31 @@
+Nhập vào tháng & năm. Hãy cho biết tháng đó trong năm đó có bao nhiêu ngày
+
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <conio.h>
+int main() {
+	int thang, nam;
+	printf("nhap thang: ");
+	scanf("%d", &thang);
+	if (thang < 0 || thang >12) {
+		printf("So thang nhap khong hop le! Xin kiem tra lai!");
+		return 0;
+	}
+	printf("nhap nam: ");
+	scanf("%d", &nam);
+	if (thang == 1 || thang == 3 || thang == 7 || thang == 8 || thang == 10 || thang == 12) {
+		printf("thang nay co 31 ngay");
+	}
+	else if (thang == 4 || thang == 6 || thang == 9 || thang == 11) {
+		printf("thang co 30 ngay");
+	}
+	else {
+		if ((thang % 4 == 0 && thang % 100 != 0) || thang % 400 == 0) {
+			printf("thang nay co 29 ngay");
+		}
+		else {
+			printf("thang nay co 28 ngay");
+		}
+	}
+	return 0;
+}
