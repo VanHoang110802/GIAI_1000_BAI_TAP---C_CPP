cb9c90d814b33fb6505d8ec34b1aaa5f08b362d3
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:02:12 +0700

    Create Bai309.cpp

---
+++ 04_MANG 1 CHIEU/Bai309.cpp
@@ -0,0 +1,61 @@
+Hãy liệt kê các số âm trong mảng một chiều các số thực.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#define Max 100
+void NhapDuLieu(float& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%f", &n);
+		if (n<1||n>Max) {
+			printf("Phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+		}
+	} while (n<1||n>Max);
+}
+void NhapMang(float a[], float n) {
+	for (int i = 0; i < n; i++) {
+		printf("a[%d]= ", i);
+		scanf("%f", &a[i]);
+	}
+}
+void XuatMang(float a[], float n) {
+	for (int i = 0; i < n; i++) {
+		printf("%.2f  ", a[i]);
+	}
+}
+void LietKeSoAm(float a[], float n) {
+	int dem = 0, i;
+	printf("\nCac so am: ");
+	for ( i = 0; i < n; i++) {
+		if (a[i] < 0) {
+			dem++;
+			break;
+		}
+	}
+	if (dem == 0) {
+		printf("0");
+	}
+	else {
+		for (i = 0; i < n; i++) {
+			if (a[i] < 0) {
+				printf("%.2f ", a[i]);
+			}
+		}
+	}
+}
+int main() {
+	float a[Max], n;
+hoang:
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	XuatMang(a, n);
+	printf("\n");
+	LietKeSoAm(a, n);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
