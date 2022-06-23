0c8f73d851d4c77f76e4235a2e14093cf502a758
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:50:27 +0700

    Create Bai433.cpp

---
+++ 04_MANG 1 CHIEU/Bai433.cpp
@@ -0,0 +1,54 @@
+Cho mảng một chiều các số nguyên a.Hãy tạo mảng b từ mảng a,sao cho mảng b chỉ chứa các giá trị lẻ .
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+#include <conio.h>
+#include<math.h>
+#define Max 100
+
+void NhapDuLieu(int& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%d", &n);
+		if (n<1||n>Max) {
+			printf("So luong phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+		}
+	} while (n<1||n>Max);
+}
+void NhapMang(int a[], int n) {
+	for (int i = 0; i < n; ++i) {
+		printf("a[%d]= ", i);
+		scanf("%d", &a[i]);
+	}
+}
+void XuatMang(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d  ", a[i]);
+	}
+}
+void TaoMangBChiChuaGiaTriLeTuMangA(int a[], int n){
+	int b[Max], nb = 0;
+	for (int i = 0; i < n; i++){	
+		if (a[i] % 2 != 0){
+			b[nb] = a[i];
+			nb++;
+		}
+	}
+	XuatMang(b, nb);
+}
+int main() {
+	int n, a[Max];
+hoang:
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	printf("\n");
+	XuatMang(a, n);
+	printf("\n");
+	TaoMangBChiChuaGiaTriLeTuMangA(a, n);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
