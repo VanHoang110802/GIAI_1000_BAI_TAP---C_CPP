9a3d9762a059bea4c527ef92167cfbe2faf34160
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:31:26 +0700

    Create Bai382.cpp

---
+++ 04_MANG 1 CHIEU/Bai382.cpp
@@ -0,0 +1,57 @@
+Hãy sắp xếp các giá trị trong mảng các số nguyên giảm dần.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#include <math.h>
+#define Max 100
+void NhapDuLieu(int& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%d", &n);
+		if (n<1||n>Max) {
+			printf("So luong phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+		}
+	} while (n<1||n>Max);
+}
+void NhapMang(int a[], int n) {
+	for (int i = 0; i < n; ++i) {
+		printf("a[%d]= ", i);
+		scanf("%d", &a[i]);
+	}
+}
+void XuatMang(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d  ", a[i]);
+	}
+}
+void HoanVi(int& a, int& b){
+	int temp = a;
+	a = b;
+	b = temp;
+}
+void SapXepGiamDan(int a[], int n){
+	for (int i = 0; i < n - 1; i++){
+		for (int j = i + 1; j < n; j++){
+			if (a[i] < a[j]){
+				HoanVi(a[i], a[j]);
+			}
+		}
+	}
+}
+int main(){
+	int n, a[Max];
+hoang:
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	XuatMang(a, n);
+	SapXepGiamDan(a, n);
+	printf("\n");
+	XuatMang(a, n);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
