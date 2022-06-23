03bc4a8ea0165e4a46ce4fa029ece8c71651f0d1
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 20:46:17 +0700

    Create Bai261.cpp

---
+++ 04_MANG 1 CHIEU/Bai261.cpp
@@ -0,0 +1,48 @@
+Viết hàm tính tổng các giá trị âm trong mảng một chiều các số thực.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#define Max 100
+void NhapDuLieu(int& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%d", &n);
+		if (n< 1 || n > Max) {
+			printf("So luong phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+		}
+	} while (n< 1 || n > Max);
+}
+void NhapMang(float a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("a[%d]= ",i);
+		scanf("%f", &a[i]);
+	}
+}
+void XuatMang(float a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%.3f ", a[i]);
+	}
+}
+float TinhTong(float a[], int n) {
+	float s = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] < 0) s += a[i];
+	}
+	return s;
+}
+int main() {
+	int n,a[Max];
+hoang:
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	XuatMang(a, n);
+	float tinh = TinhTong(a, n);
+	printf("\nTong = %.2f", tinh);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
