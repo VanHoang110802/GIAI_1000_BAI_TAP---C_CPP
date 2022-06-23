d4d1a08190129bfa409f529b52ec4976c57ef736
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:24:19 +0700

    Create Bai362.cpp

---
+++ 04_MANG 1 CHIEU/Bai362.cpp
@@ -0,0 +1,50 @@
+ Hãy đếm số lượng giá trị có chữ số tận cùng bằng 5 trong mảng các số nguyên .
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#define Max 100
+void NhapDuLieu(int& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%d", &n);
+		if (n<1||n>Max) {
+			printf("So luong phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+		}
+	} while (n<1||n>Max);
+}
+void NhapMang(int a[], int n) {
+	for (int i = 0; i < n; ++i) {
+		printf("a[%d]= ", i);
+		scanf("%d", &a[i]);
+	}
+}
+void XuatMang(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d  ", a[i]);
+	}
+}
+int DemSoLuotGiaTriTanCungLa5(int a[], int n){
+	int dem = 0;
+	for (int i = 0; i < n; i++){
+		if (a[i] % 10 == 5){
+			dem++;
+		}
+	}
+	return dem;
+}
+int main(){
+	int n, a[Max];
+hoang:
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	XuatMang(a, n);
+	int kt = DemSoLuotGiaTriTanCungLa5(a, n);
+	printf("\nCo %d so luong thoa man.", kt);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
