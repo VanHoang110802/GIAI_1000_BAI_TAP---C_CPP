8e74ec1a2671b2bd89ea77f8e922981572e929de
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:19:16 +0700

    Create Bai351.cpp

---
+++ 04_MANG 1 CHIEU/Bai351.cpp
@@ -0,0 +1,54 @@
+Hãy kiểm tra mảng có tăng dần hay không ?
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#define Max 100
+void NhapDuLieu(int& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%d", &n);
+		if (n<1||n>Max) {
+			printf("So luong phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+		}
+	} while (n<1||n>Max);
+}
+void NhapMang(int a[], int n) {
+	for (int i = 0; i < n; ++i) {
+		printf("a[%d]= ", i);
+		scanf("%d", &a[i]);
+	}
+}
+void XuatMang(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d  ", a[i]);
+	}
+}
+int KiemTraTangDan(int a[], int n){
+	int check = 1;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (a[i] > a[i + 1])
+		{
+			check = 0;
+			break;
+		}
+	}
+	return check;
+}
+int main(){
+	int n, a[Max];
+hoang:
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	XuatMang(a, n);
+	printf("\nKiem tra mang co tang dan hay khong?\n1-CO\n0-KHONG CO\n");
+	int kt = KiemTraTangDan(a, n);
+	printf("\n%d", kt);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
