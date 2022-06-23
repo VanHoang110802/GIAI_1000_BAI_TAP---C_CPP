24cc89d2d7a9a3f3105862933e701c2acdeaef22
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 19:04:04 +0700

    Create Bai095.c

---
+++ 02_VONG LAP/Bai095.c
@@ -0,0 +1,32 @@
+Tính S(n) = 𝑛+1√𝑛! + 𝑛√(𝑛 − 1)! + 3√2! + √1! có n dấu căn.
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+hoang:
+    int i, n;
+    float S, M;
+    do
+    {
+        printf("Nhap n= ");
+        scanf("%d", &n);
+        if(n < 1){
+            printf("n phai >= 1. Xin nhap lai !\n");
+        }
+    }while(n < 1);
+ 
+    float s=sqrt(1.0);
+	long s1=1;
+	for(int i=2;i<=n;i++){
+		s1*=i; 		// Tính ra giai thua cua i .
+		s=pow(1.0*(s1+s),1.0/(i+1));
+	}
+	printf("\nKet qua la: %.3f",s);
+    printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+    return 0;
+}
