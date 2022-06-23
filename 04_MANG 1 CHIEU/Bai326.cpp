8abd799599f057b592d71bd038e432d6541af548
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:09:50 +0700

    Create Bai326.cpp

---
+++ 04_MANG 1 CHIEU/Bai326.cpp
@@ -0,0 +1,3 @@
+Viết chương trình nhập vào mảng các số nguyên và sử dụng lần lượt các thuật toán tìm kiếm Tuần tự vét cạn (LinearExhaustive),thuật toán tìm kiếm tuần tự lính canh (LinearSentinel) và thuật toán tìm kiếm nhị phân (BinarySearch) (Google để xem và hiểu về thuật toán này nên xem wiki - cấm xem code giải).
+
+		*Bài này thuộc kiến thức cao hơn, méo làm :vv
