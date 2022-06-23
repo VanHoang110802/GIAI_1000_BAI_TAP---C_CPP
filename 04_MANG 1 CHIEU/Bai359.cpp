89cf45b87e1300b79f9ba12898494164b7a8c338
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 21:22:53 +0700

    Create Bai359.cpp

---
+++ 04_MANG 1 CHIEU/Bai359.cpp
@@ -0,0 +1,51 @@
+Đếm số lượng giá trị dương chia hết cho 7 trong mảng một chiều các số nguyên .
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <conio.h>
+#define Max 100
+void NhapDuLieu(int &n){
+    do{
+        printf("Nhap so luong phan tu: ");
+        scanf("%d",&n);
+        if(n<1||n>Max){
+            printf("So luong phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+        }
+    }while(n<1||n>Max);
+}
+void NhapMang(int a[], int n){
+    for(int i=0;i<n;++i){
+        printf("a[%d]= ",i);
+        scanf("%d",&a[i]);
+    }
+}
+void XuatMang(int a[], int n){
+    for(int i=0;i<n;i++){
+        printf("%d  ", a[i]);
+    }
+}
+int DemSoChiaHetCho7(int a[], int n){
+    int dem = 0;
+    for(int i=0;i<n;i++){
+        if(a[i] % 7 == 0){
+            dem++;
+        }
+    }
+    return dem;
+}
+int main(){
+    int a[Max], n;
+hoang:
+    NhapDuLieu(n);
+    NhapMang(a,n );
+    XuatMang(a,n);
+    printf("\n");
+    int dem = DemSoChiaHetCho7(a,n);
+    printf("\nCo %d so luong thoa man.", dem);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+    return 0;
+}
