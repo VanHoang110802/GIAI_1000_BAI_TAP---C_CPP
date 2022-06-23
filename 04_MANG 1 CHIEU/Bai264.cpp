251b3146ec83d677e0cf8d91aa409c5773a126b6
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 20:47:35 +0700

    Create Bai264.cpp

---
+++ 04_MANG 1 CHIEU/Bai264.cpp
@@ -0,0 +1,50 @@
+Viết hàm tìm "giá trị lớn nhất" trong mảng một chiều các số thực.
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#define Max 100
+void NhapDuLieu(int& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%d", &n);
+		if (n<1||n>Max) {
+			printf("So luong phan tu nhap khong hop le, xin hay kiem tra lai!\n");
+		}
+	} while (n<1||n>Max);
+}
+void NhapMang(float a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("a[%d]= ", i);
+		scanf("%f", &a[i]);
+	}
+}
+void XuatMang(float a[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%.2f ", a[i]);
+	}
+}
+float TimGiaTriMax(float a[], int n) {
+	int max = a[0];
+	for (int i = 0; i < n; i++) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+	return max;
+}
+int main() {
+	int n;
+	float a[Max];
+hoang:
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	XuatMang(a, n);
+	float max = TimGiaTriMax(a, n);
+	printf("\nPhan tu lon nhat trong mang: %.1f", max);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
