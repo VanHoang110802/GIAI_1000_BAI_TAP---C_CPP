6d3c1d45bf582ba5918f6b0870910afc8ff69986
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 19:09:56 +0700

    Create Bai113.cpp

---
+++ 02_VONG LAP/Bai113.cpp
@@ -0,0 +1,38 @@
+Hãy kiểm tra số nguyên dương n có toàn chữ số lẻ hay không?
+/*
+n = 123456789 -> n không có toàn chữ số lẻ
+n = 1357913-> n toàn chữ số lẻ
+*/
+#include <stdio.h>
+#include <conio.h>
+int main(){
+hoang:
+	int n;
+	do{
+		printf("Nhap n= ");
+		scanf("%d",&n);
+		if(n<=0){
+			printf("n > 0, xin hay kiem tra lai!\n");
+		}
+	}while(n<=0);
+	int temp = n, check = 1;
+	while(temp != 0){
+		int cs = temp % 10;
+		temp /= 10;
+		if(cs % 2 == 0){
+			check = 0;
+			break;
+		}
+	}
+	if(check == 1){
+		printf("n toan chu so le!");
+	} else {
+		printf("n co chu so chan!");
+	}
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
