f7109dc50191dc8d95897050b9d87cdb52f1187f
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-23 20:51:32 +0700

    Create Bai278.cpp

---
+++ 04_MANG 1 CHIEU/Bai278.cpp
@@ -0,0 +1,48 @@
+Tìm "số dương cuối cùng" trong mảng một chiều các số thực.Nếu mảng không có giá trị dương thì trả về giá trị -1. 
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<conio.h>
+#define Max 100
+void NhapDuLieu(float& n) {
+	do {
+		printf("Nhap so luong phan tu: ");
+		scanf("%f", &n);
+		if (n<1||n>Max) {
+			printf("So luong phan tu nhap khong hop le!\n");
+		}
+	} while (n<1||n>Max);
+}
+void NhapMang(float a[], float n) {
+	for (int i = 0; i < n; i++) {
+		printf("a[%d]= ", i);
+		scanf("%f", &a[i]);
+	}
+}
+void XuatMang(float a[], float n) {
+	for (int i = 0; i < n; i++) {
+		printf("%.2f  ", a[i]);
+	}
+}
+float TimSoDuongCuoi(float a[], float n) {
+	for (int i = n-1; i >= 0; i--) {
+		if (a[i] > 0) return a[i];
+	}
+	return -1;
+}
+int main() {
+	float a[Max], n;
+hoang:
+	NhapDuLieu(n);
+	NhapMang(a, n);
+	XuatMang(a, n);
+	printf("\n");
+	float tim = TimSoDuongCuoi(a, n);
+	printf("So duong cuoi cung: %.2f", tim);
+	printf("\n\nBan co muon tiep tuc hay khong?\n(Bam phim c de tiep tuc, bam phim bat ki de thoat.)\n");
+	char lc = getch();
+	if(lc == 'c' || lc == 'C'){
+		goto hoang;
+	}
+	return 0;
+}
