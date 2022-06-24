505cc06d1d0d7d6ea36ed8752d8fd220fb61038d
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-24 09:41:14 +0700

    Create Bai461.cpp
    Signed-off-by: Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>

---
+++ 05_CHUOI/Bai461.cpp
@@ -0,0 +1,44 @@
+Viết hàm nhận vào một chuỗi s và trả về chuỗi tương ứng sau khi xóa các khoảng trắng.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void XoaKyTu(char *s, int vitrixoa){
+	int length = strlen(s);
+	for(int i= vitrixoa + 1; i< length;i++){
+		s[i-1] = s[i];
+	}
+	s[length -1] = '\0';									
+}
+char *ChuanHoaChuoi(char *s){
+	// tạo bản sao cho chuỗi vì phải giữ nguyên đầu vào.
+	char *p = strdup(s);
+	int length = strlen(p);
+	for(int i= 0; i< length;i++){
+		if(p[i] == ' ' && p[i+1] == ' '){
+			XoaKyTu(p,i);
+			i--;
+			length--;	// mỗi lần phải xóa xong phải cập nhật lại( do bên mảng nó sẽ chạy theo n, còn ở đây chạy theo length nên ta phải cập nhật lại cho nó).
+		}
+	}
+	if(p[0] == ' '){
+		XoaKyTu(p,0);
+		length--;
+	}
+	if(p[length - 1] == ' '){
+		XoaKyTu(p,length - 1);
+	}
+	return p;
+}
+
+int main(){
+	char s[] = " Hoang   van  cu phai        la  dep   trai    vai o       ";
+	char p[30];
+	strcpy(p, ChuanHoaChuoi(s));	// gán chuỗi s sang cho p
+	printf("\n%s",s);
+	printf("\n%s",p);
+	//XoaKyTu(s,0);
+	//printf("\n%s",s);
+	return 0;
+}
