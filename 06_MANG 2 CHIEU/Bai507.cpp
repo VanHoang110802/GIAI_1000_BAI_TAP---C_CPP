0be94034c449da6d9c8b17cbb643cd2fbafd6904
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-24 14:00:50 +0700

    Create Bai507.cpp
    Signed-off-by: Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>

---
+++ 06_MANG 2 CHIEU/Bai507.cpp
@@ -0,0 +1,79 @@
+Tính tổng các số dương trong ma trận các số thực.
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define MAX 100
+void NhapMang(float a[][MAX], int &dong, int &cot)
+{
+	//Nhập số dòng
+	do
+	{
+		printf("\nNhap vao so dong: ");
+		scanf("%d",&dong);
+		if(dong < 1 || dong > MAX)
+		{
+			printf("\nSo dong khong hop le. Xin kiem tra lai!\n");
+		}
+
+	}while(dong < 1 || dong > MAX);
+
+	//Nhập số cột
+	do
+	{
+		printf("\nNhap vao so cot: ");
+		scanf("%d",&cot);
+
+		if(cot < 1 || cot > MAX)
+		{
+			printf("\nSo cot khong hop le. Xin kiem tra lai!\n");
+
+		}
+
+	}while(cot < 1 || cot > MAX);
+	for(int i = 0; i < dong; i++)
+	{
+		for(int j = 0; j < cot; j++)
+		{
+			printf("a[%d][%d] = ", i, j);
+			scanf("%f", &a[i][j]);
+		}
+	}
+}
+
+void XuatMang(float a[][MAX], int dong, int cot)
+{
+	for(int i = 0; i < dong; i++)
+	{
+		for(int j = 0; j < cot; j++)
+		{
+			printf("%.3f ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+float TinhTongCacSoDuong(float a[][MAX], int dong, int cot)
+{
+	float tong = 0;
+
+	int n = dong * cot; // Tổng số lượng phần tử của mảng
+	for(int i = 0; i < n; i++)
+	{
+		if(a[i / cot][i % cot] > 0)
+		{
+			tong += a[i / cot][i % cot];
+		}
+	}
+	return tong;
+}
+int main()
+{
+	float a[MAX][MAX];
+	int dong, cot;
+	NhapMang(a, dong, cot);
+	XuatMang(a, dong, cot);
+	float tong = TinhTongCacSoDuong(a, dong, cot);
+	printf("\nTong cac so duong trong mang la: %.2f", tong);
+	return 0;
+}
