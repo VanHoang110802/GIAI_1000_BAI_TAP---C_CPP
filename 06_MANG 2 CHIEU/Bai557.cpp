a4eb0c70d9f1ecdf23b7d4e0ea68343b83597e9e
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-24 14:20:12 +0700

    Create Bai557.cpp
    Signed-off-by: Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>

---
+++ 06_MANG 2 CHIEU/Bai557.cpp
@@ -0,0 +1,77 @@
+Tìm giá trị lớn nhất trong ma trận.
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define MAX 100
+void NhapMang(int a[][MAX], int &dong, int &cot)
+{
+	//Nhập số dòng
+	do
+	{
+		printf("\nNhap vao so dong: ");
+		scanf("%d",&dong);
+
+		if(dong < 1 || dong > MAX)
+		{
+			printf("\nSo dong khong hop le. Xin kiem tra lai!\n");
+		}
+
+	}while(dong < 1 || dong > MAX);
+
+	//Nhập số cột
+	do
+	{
+		printf("\nNhap vao so cot: ");
+		scanf("%d",&cot);
+
+		if(cot < 1 || cot > MAX)
+		{
+			printf("\nSo cot khong hop le. Xin kiem tra lai!\n");
+
+		}
+
+	}while(cot < 1 || cot > MAX);
+	for(int i = 0; i < dong; i++)
+	{
+		for(int j = 0; j < cot; j++)
+		{
+			printf("a[%d][%d] = ", i, j);
+			scanf("%d", &a[i][j]);
+		}
+	}
+}
+
+void XuatMang(int a[][MAX], int dong, int cot)
+{
+	for(int i = 0; i < dong; i++)
+	{
+		for(int j = 0; j < cot; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int TimMaxTrongMaTran(int a[][MAX], int dong, int cot) 
+{ 
+	int Max = a[0][0]; 
+	for (int i = 0; i < dong; i++) 
+	{ 
+		for(int j = 0; j < cot; j++) 
+		{ 
+			Max = (a[i][j] > Max) ? a[i][j] : Max; 
+		} 
+	} 
+	return Max; 
+} 
+int main()
+{
+	int a[MAX][MAX], dong, cot;
+	NhapMang(a, dong, cot);
+	XuatMang(a, dong, cot);
+	int max = TimMaxTrongMaTran(a, dong, cot);
+	printf("\nPhan tu lon nhat trong ma tran = %d", max);
+	return 0;
+}
