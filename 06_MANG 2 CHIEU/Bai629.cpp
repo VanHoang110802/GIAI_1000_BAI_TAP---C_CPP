695eda2bc07790645c8a4e84bac26640d81b1631
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-24 14:46:59 +0700

    Create Bai629.cpp
    Signed-off-by: Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>

---
+++ 06_MANG 2 CHIEU/Bai629.cpp
@@ -0,0 +1,73 @@
+Đếm số lượng dòng giảm trong ma trận.
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define MAX 100
+void NhapMaTran(int a[][MAX], int &n)
+{
+	do
+	{
+		printf("\nNhap n: ");
+		scanf("%d", &n);
+		if(n < 1 || n > MAX)
+		{
+			printf("\nSo phan tu khong hop le. Xin kiem tra lai !\n");
+		}
+	}while(n < 1 || n > MAX);
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			printf("a[%d][%d] = ", i, j);
+			scanf("%d", &a[i][j]);
+		}
+	}
+}
+
+void XuatMaTran(int a[][MAX], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int KiemTraDongGiam(int a[], int n)
+{
+	for(int i = 0; i < n - 1; i++)
+	{
+		if(a[i] < a[i + 1])
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int DemSoLuongDongGiam(int a[][MAX], int n)
+{
+	int dem = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(KiemTraDongGiam(a[i], n) == 1)
+		{
+			dem++;
+		}
+	}
+	return dem;
+}
+int main()
+{
+	int a[MAX][MAX], n;
+	NhapMaTran(a,n);
+	XuatMaTran(a,n);
+	int dem = DemSoLuongDongGiam(a, n);
+	printf("\nSo luong dong giam la: %d", dem);
+	return 0;
+}
