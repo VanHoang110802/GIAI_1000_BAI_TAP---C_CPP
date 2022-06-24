6ecbb696243d36c2ce42f5beb8fa9a4159e0affd
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-24 14:55:19 +0700

    Create Bai652.cpp
    Signed-off-by: Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>

---
+++ 06_MANG 2 CHIEU/Bai652.cpp
@@ -0,0 +1,1 @@
+Cho ma trận các số thực A(m x n). Hãy xây dựng ma trận B(m x n) từ ma trận A sao cho B[i][j] = số lượng phần tử dương xung quanh A[i][j] trong ma trận A (B[i][j] tối đa là 8 và nhỏ nhất là 0).
