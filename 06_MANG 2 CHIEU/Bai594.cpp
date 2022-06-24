c200f12bdd459f87136087385c927763b70921e0
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-24 14:33:45 +0700

    Create Bai594.cpp
    Signed-off-by: Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>

---
+++ 06_MANG 2 CHIEU/Bai594.cpp
@@ -0,0 +1,93 @@
+Viết hàm sắp xếp các phần tử trên một dòng tăng dần từ trái sang phải.
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define MAX 100
+void NhapMang(int a[][MAX], int &dong, int &cot)
+{
+	//Nhập số dòng
+	do
+	{
+		printf("\nNhap vao so dong: ");
+		scanf("%d",&dong);
+		if(dong < 1 || dong > MAX)
+		{
+			printf("\nSo dong khong hop le. Xin kiem tra lai!\n");
+		}
+
+	}while(dong < 1 || dong > MAX);
+
+	//Nhập số cột
+	do
+	{
+		printf("\nNhap vao so cot: ");
+		scanf("%d",&cot);
+
+		if(cot < 1 || cot > MAX)
+		{
+			printf("\nSo cot khong hop le. Xin kiem tra lai!\n");
+
+		}
+
+	}while(cot < 1 || cot > MAX);
+	for(int i = 0; i < dong; i++)
+	{
+		for(int j = 0; j < cot; j++)
+		{
+			printf("a[%d][%d] = ", i, j);
+			scanf("%d", &a[i][j]);
+		}
+	}
+}
+
+void XuatMang(int a[][MAX], int dong, int cot)
+{
+	for(int i = 0; i < dong; i++)
+	{
+		for(int j = 0; j < cot; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void HoanVi(int &a, int &b)
+{
+	a = a + b;
+	b = a - b;
+	a = a - b;
+}
+
+void SapXepPhanTuTren1DongTangDanTuTraiSangPhai(int a[][MAX], int dong, int cot, int x)
+{
+	for(int i = 0; i < cot - 1; i++)
+	{
+		for(int j = i + 1; j < cot; j++)
+		{
+			if(a[x][i] > a[x][j])
+			{
+				HoanVi(a[x][i], a[x][j]);
+			}
+		}
+	}
+}
+int main()
+{
+	int a[MAX][MAX], dong, cot, x;
+	NhapMang(a, dong, cot);
+	XuatMang(a, dong, cot);
+	do{
+		printf("\nNhap dong muon sap xep: ");
+		scanf("%d", &x);
+		if(x < 0 || x > dong - 1)
+		{
+			printf("\nSo phan tu khong hop le. Xin kiem tra lai!\n");
+		}
+	}while(x < 0 || x > dong - 1);
+	SapXepPhanTuTren1DongTangDanTuTraiSangPhai(a, dong, cot, x);
+	printf("\nMa tran sau khi sap xep: \n");
+	XuatMang(a, dong, cot);
+	return 0;
+}
