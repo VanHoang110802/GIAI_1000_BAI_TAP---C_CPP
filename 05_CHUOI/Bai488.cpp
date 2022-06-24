4dd4e82c65a769a373450cf4370ec9bfd10d8532
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-06-24 09:55:31 +0700

    Create Bai488.cpp
    Signed-off-by: Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>

---
+++ 05_CHUOI/Bai488.cpp
@@ -0,0 +1,2 @@
+Cho 2 dãy nhị phân A và B. Hãy thực hiện phép A xor B ( phép xor tương đương với toán tử ^ trong một số các ngôn ngữ lập trình ) và in ra kết quả của phép xor đó.
+input: 1010 1100 -> output: 0110
