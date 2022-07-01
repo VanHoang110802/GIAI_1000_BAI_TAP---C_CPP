67bad4d1157a544cedccda50b5bd9ba59f019cc6
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-01 12:02:31 +0700

    Create main.c
    Signed-off-by: Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>

---
+++ 09_TAP TIN/Bai964/main.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+
+void convertFile(FILE* inFile, FILE* outFile)
+{
+	int ch;
+	while (1)
+	{
+		ch = fgetc(inFile);
+		if (!feof(inFile))
+		{
+			ch = toupper(ch);
+			fputc(ch, outFile);
+		}
+		else
+			break;
+	}
+}
+
+int main()
+{
+	FILE* fpIn, * fpOut;
+	const char* fname = "Data.txt"; 
+	const char* fcopy = "DataUpper.txt";
+	fpIn = fopen(fname, "rt");
+	if (fpIn == NULL)
+	{
+		printf("File %s not found!\n", fname);
+		return 0;
+	}
+	fpOut = fopen(fcopy, "wt");
+	if (fpOut == NULL)
+	{
+		printf("Can not open file %s!\n", fcopy);
+		fclose(fpIn);
+		return 0;
+	}
+	convertFile(fpIn, fpOut);
+	fclose(fpIn);
+	fclose(fpOut);
+	return 0;
+}
