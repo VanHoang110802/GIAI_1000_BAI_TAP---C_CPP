69cf605d510ecd930460f0a295bbcf595b669711
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-01 12:09:17 +0700

    Create main.c
    Signed-off-by: Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>

---
+++ 09_TAP TIN/Bai965/main.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<conio.h>
+
+int KiemTraFileCoTonTai(const char* fname)
+{
+	FILE* fp = fopen(fname, "rb");
+	if (fp == NULL)
+	{
+		return 0;
+	}
+	fclose(fp);
+	return 1;
+}
+
+int KiemTraFileCoDocDuoc(const char* fname)
+{
+	FILE* fp = fopen(fname, "r + b");
+	if (fp == NULL)
+	{
+		return 1;
+	}
+	fclose(fp);
+	return 0;
+}
+void CheckFile(const char* fname)
+{
+	if (KiemTraFileCoTonTai(fname))
+	{
+		printf("File %s dang ton tai.\n", fname);
+		if (KiemTraFileCoDocDuoc(fname))
+		{
+			printf("File %s chi co the doc\n", fname);
+		}
+		else
+		{
+			printf("File %s co the ghi va doc\n", fname);
+		}
+	}
+	else
+	{
+		printf("File %s khong tim thay\n", fname);
+	}
+}
+int main()
+{
+	const char* fname1 = "Result.txt"; 
+	const char* fname2 = "Data.txt";
+	const char* fname3 = "WELCOME.txt";
+	CheckFile(fname1);
+	CheckFile(fname2);
+	CheckFile(fname3);
+
+	return 0;
+}
