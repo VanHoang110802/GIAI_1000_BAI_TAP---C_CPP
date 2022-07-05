1e99863a134919afc56c5f48bd9df339573d4a18
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-05 10:06:09 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai825/main.cpp
@@ -0,0 +1,15 @@
+/*
+Hãy khai báo kiểu dữ liệu biểu diễn khái niệm điểm trong không gian Oxyz và định nghĩa hàm nhập,
+hàm xuất cho kiểu dữ liệu này
+
+*/
+
+#include"DiemTrongMP.h"
+
+int main()
+{
+	DIEM d;
+	NhapDiem(d);
+	XuatDiem(d);
+	return 0;
+}
