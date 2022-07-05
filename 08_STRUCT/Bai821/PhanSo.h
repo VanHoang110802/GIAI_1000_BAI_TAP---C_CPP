5bd5ed506de39b9c3fc6211d8c3984eb712401b3
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-05 07:05:11 +0700

    Create PhanSo.h

---
+++ 08_STRUCT/Bai821/PhanSo.h
@@ -0,0 +1,15 @@
+#ifndef _PHANSO_
+#define _PHANSO_
+
+#include <iostream>
+using namespace std;
+struct PhanSo
+{
+	int tuso, mauso;
+};
+typedef struct PhanSo PHANSO;
+
+void NhapPS(PHANSO&);
+void XuatPS(PHANSO);
+
+#endif
