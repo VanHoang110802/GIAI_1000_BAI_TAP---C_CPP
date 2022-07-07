ab71cb76cea580dfa1e428586365b6ee179e50c4
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-07 10:47:30 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai829/main.cpp
@@ -0,0 +1,14 @@
+/*
+Hãy khai báo kiểu dữ liệu biểu diễn khái niệm đường thằng ax + by + c = 0 trong mặt phẳng Oxy và định nghĩa
+hàm nhập, hàm xuất cho kiểu dữ liệu này.
+*/
+#include <conio.h>
+#include"DuongThang.h"
+
+int main()
+{
+	DUONGTHANG dt;
+	NhapDuongThang(dt);
+	XuatDuongThang(dt);
+	return 0;
+}
