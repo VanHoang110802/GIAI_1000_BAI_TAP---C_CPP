6418b6eeca7a1372a02e2b84e6af1024c670a916
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-07 11:53:12 +0700

    Create Diem.h

---
+++ 08_STRUCT/Bai832/Diem.h
@@ -0,0 +1,17 @@
+#ifndef _diem_
+#define _diem_
+
+#include <iostream>
+using namespace std;
+
+struct Diem
+{
+	float x, y;
+};
+typedef struct Diem DIEM;
+
+void NhapDiem(DIEM&);
+void XuatDiem(DIEM);
+float TinhKhoangCach(DIEM, DIEM);
+
+#endif 
