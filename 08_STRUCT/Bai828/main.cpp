8db87642023472ede444e81c3152c3a388dcee1f
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-07 09:34:05 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai828/main.cpp
@@ -0,0 +1,14 @@
+/*
+Hãy khai báo kiểu dữ liệu biểu diễn ngày, tháng, năm trong thế giới thực và định nghĩa hàm nhập, hàm xuất cho kiểu dữ liệu này.
+
+*/
+
+#include"NgayThangNam.h"
+
+int main()
+{
+	THOIGIAN tg;
+	NhapThoiGian(tg);
+	XuatThoiGian(tg);
+	return 0;
+}
