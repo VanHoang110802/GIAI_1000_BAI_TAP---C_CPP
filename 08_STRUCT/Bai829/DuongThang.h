c04bfcd4884cdeb8ca94f0ff487f1e1b7544eeee
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-07 10:47:54 +0700

    Create DuongThang.h

---
+++ 08_STRUCT/Bai829/DuongThang.h
@@ -0,0 +1,17 @@
+#ifndef _duongthangOXY_
+#define _duongthangOXY_
+
+#include <iostream>
+using namespace std;
+
+struct DuongThang
+{
+    float a, b, c;
+};
+typedef DuongThang DUONGTHANG;
+
+void NhapDuongThang(DUONGTHANG&);
+void XuatDuongThang(DUONGTHANG);
+
+#endif 
+
