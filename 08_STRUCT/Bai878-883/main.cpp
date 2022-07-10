2b8b09e06dde6596b9e68dde6c6a98f28a8ccc12
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:40:55 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai878-883/main.cpp
@@ -0,0 +1,10 @@
+/*
+Khai báo kiểu dữ liệu để biểu diễn thông tin của một số phức.
+Nhập số phức.
+Xuất số phức
+Tính tổng hai số phức.
+Tính hiệu hai số phức.
+Tính tích hai số phức.
+Tính thương hai số phức.
+Tính lũy thừa bậc n của số phức.
+*/
