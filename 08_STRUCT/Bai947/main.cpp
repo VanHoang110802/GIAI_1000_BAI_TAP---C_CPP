7b4df0478f568a83c6ef1c84fc15907cf3b24c08
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:02:02 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai947/main.cpp
@@ -0,0 +1,9 @@
+/*
+Hãy khai báo kiểu dữ liệu biểu diễn thông tin của một chuyến bay. Biết rằng một chuyến bay gồm những thành phần như sau:
+- Mã chuyến bay: chuỗi tối đa 5 ký tự.
+- Ngày bay: kiểu dữ liệu ngày.
+- Giờ bay: kiểu dữ liệu thời gian.
+- Nơi đi: chuỗi tối đa 20 kí tự.
+- Nơi đến: chuỗi tối đa 20 kí tự.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
