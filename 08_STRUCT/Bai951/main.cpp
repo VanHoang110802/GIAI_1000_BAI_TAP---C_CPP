63c9aa3bbb5b4806c61848d77a0915c5cff19fdb
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:06:11 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai951/main.cpp
@@ -0,0 +1,10 @@
+/*
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một thí sinh. Biết rằng một thí sinh gồm những thành phần như sau:
+- Mã thí sinh: chuỗi tối đa 30 kí tự.
+- Họ tên thí sinh: chuỗi tối đa 30 kí tư.
+- Điểm toán: kiểu số thực.
+- Điểm lý: kiểm số thực.
+- Điểm hóa: kiểu số thực.
+- Điểm tổng cộng: kiểu số thực.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
