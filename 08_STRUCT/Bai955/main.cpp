00c6d9b4213ad16fc59dcab26c18fbcb62743924
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:07:27 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai955/main.cpp
@@ -0,0 +1,9 @@
+/*
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một luận văn. Biết rằng một luận văn gồm những thành phần như sau:
+- Mã luận văn: chuỗi tối đa 10 kí tự.
+- Tên luận văn: chuỗi tối đa 100 kí tự.
+- Họ tên thí sinh viên thực hiện: chuỗi tối đa 30 kí tự.
+- Họ tên giáo viên hướng dẫn: chuỗi tối đa 30 kí tự.
+- Năm thực hiện: kiểu số nguyên 2 byte.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
