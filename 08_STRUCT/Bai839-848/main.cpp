45c18e3613b1c5db28610d84ed1920db1422e6af
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 11:24:29 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai839-848/main.cpp
@@ -0,0 +1,14 @@
+/*
+
+Tính tổng, hiệu, tích, thương hai đa thức.
+Tính đa thức dư của phép chia đa thức thứ nhất cho đa thức thứ hai.
+Tính đạo hàm cấp 1 của đa thức.
+Tính đạo hàm cấp k của đa thức.
+Tính giá trị của đa thức tại vị trí x = xo.
+Định nghĩa toán tử cộng (operator +) cho hai đa thức.
+Định nghĩa toán tử hiệu (operator –) cho hai đa thức.
+Định nghĩa toán tử tích (operator *) cho hai đa thức.
+Định nghĩa toán tử thương (operator /) cho hai đa thức.
+Tính nghiệm của đa thức trong đoạn [a, b] cho trước.
+
+*/
