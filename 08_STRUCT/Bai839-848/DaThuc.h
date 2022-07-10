32c88d681c0c1aa34b26ac2cb73ef9a6773c8a5f
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:12:40 +0700

    Create DaThuc.h

---
+++ 08_STRUCT/Bai839-848/DaThuc.h
@@ -0,0 +1,27 @@
+#ifndef _dathuc_
+#define _dathuc_
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+struct DaThuc
+{
+	float* heso;
+	int sobac;
+};
+typedef struct DaThuc DATHUC;
+/* Còn toán tử operator thì quá dễ rồi, mỗi hàm tính toán, các thứ thêm vào operator là xong */
+void NhapDaThuc(DATHUC*);
+void XuatDaThuc(DATHUC*);
+void* TinhTong2DT(DATHUC*, DATHUC*);
+void* TinhHieu2DT(DATHUC*, DATHUC*);
+void* TinhTich2DT(DATHUC*, DATHUC*);
+void* TinhThuong2DT(DATHUC*, DATHUC*);
+//void* TinhDaoHamCap1(DATHUC*);  // nếu chỉ tính 1 cấp đạo hàm cho đa thức thì dùng cái này
+DATHUC* TinhDaoHamCap1(DATHUC*);	// nếu tính cấp n đạo hàm thì tính ở đây
+void* TinhDaoHamCapk(DATHUC* , int);
+//void* TinhTichPhanCapKDaThuc(DATHUC*, int);
+float TinhGiaTriX(DATHUC*, float );
+
+#endif
