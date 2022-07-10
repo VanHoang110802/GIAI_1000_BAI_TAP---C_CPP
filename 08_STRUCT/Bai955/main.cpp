33afa3f67e54ce783252cf95cbccd381d9532341
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:08:06 +0700

    Update main.cpp

--- 08_STRUCT/Bai955/main.cpp
+++ 08_STRUCT/Bai955/main.cpp
@@ -1,9 +1,8 @@
 /*
-Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một luận văn. Biết rằng một luận văn gồm những thành phần như sau:
-- Mã luận văn: chuỗi tối đa 10 kí tự.
-- Tên luận văn: chuỗi tối đa 100 kí tự.
-- Họ tên thí sinh viên thực hiện: chuỗi tối đa 30 kí tự.
-- Họ tên giáo viên hướng dẫn: chuỗi tối đa 30 kí tự.
-- Năm thực hiện: kiểu số nguyên 2 byte.
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một học sinh (HOCSINH). Biết rằng một học sinh gồm những thành phần như sau:
+- Tên học sinh: chuỗi tối đa 30 ký tự.
+- Điểm toán: kiểu số nguyên 2 byte.
+- Điểm văn: kiểu số nguyên 2 byte.
+- Điểm trung bình: kiểu số thực.
 Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
 */
