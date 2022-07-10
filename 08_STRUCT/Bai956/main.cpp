5c430ac07c99f2c7358a5ff811c64ce4886c9ac8
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:08:58 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai956/main.cpp
@@ -0,0 +1,7 @@
+/*
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một lớp học. Biết rằng một lớp học gồm những thành phần như sau:
+- Tên lớp: chuỗi tối đa 30 ký tự.
+- Sĩ số: kiểu số nguyên 2 byte.
+- Danh sách các học sinh trong lớp (tối đa 50 học sinh).
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
