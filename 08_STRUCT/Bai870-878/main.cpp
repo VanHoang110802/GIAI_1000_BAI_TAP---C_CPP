cb8d2d7a676115848a7ee937e9d435844c08c4f6
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:39:43 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai870-878/main.cpp
@@ -0,0 +1,12 @@
+/*
+Viết khai báo kiểu dữ liệu để biểu diễn thông tin của một hỗn số
+Nhập hỗn số.
+Xuất hỗn số.
+Rút gọn hỗn số.
+Tính tổng hai hỗn số.
+Tính hiệu hai hỗn số.
+Tính tích hai hỗn số.
+Tính thương hai hỗn số.
+Kiểm tra hỗn số tối giản.
+Qui đồng hay hỗn số.
+*/
