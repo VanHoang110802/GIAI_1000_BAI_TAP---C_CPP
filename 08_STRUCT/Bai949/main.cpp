616b519a6b300991fc071a426e1fab08db7adb7c
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:04:49 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai949/main.cpp
@@ -0,0 +1,7 @@
+/*
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một đội bóng. Biết rằng một đội bóng gồm những thành phần như sau:
+- Mã đội bóng: chuỗi tối đa 5 ký tự.
+- Tên đội bóng: chuỗi tối đa 30 ký tự.
+- Danh sách các cầu thủ: mảng một chiều các cầu thủ (tối đa 30 phần tử).
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
