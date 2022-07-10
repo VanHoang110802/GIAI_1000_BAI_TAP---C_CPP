c99b594752d75c51e540f8b488e1f34e20e0c6e3
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:44:13 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai899-910/main.cpp
@@ -0,0 +1,14 @@
+/*
+Điểm trong không gian Oxyz
+Khai báo kiểu dữ liệu biểu diễn tọa độ một điểm trong không gian Oxyz.
+Nhập toạ độ điểm trong không gian Oxyz.
+Xuất toạ độ điểm theo định dạng (x, y, z).
+Tính khoảng cách giữa hai điểm trong không gian.
+Tính khoảng cách giữa hai điểm trong không gian theo phương x.
+Tính khoảng cách giữa hai điểm trong không gian theo phương y.
+Tính khoảng cách giữa hai điểm trong không gian theo phương z.
+Tìm toạ độ điểm đối xứng qua gốc toạ độ.
+Tìm toạ độ điểm đối xứng qua mặt phẳng Oxy.
+Tìm toạ độ điểm đối xứng qua mặt phẳng Oxz.
+Tìm toạ độ điểm đối xứng qua mặt phẳng Oyz.
+*/
