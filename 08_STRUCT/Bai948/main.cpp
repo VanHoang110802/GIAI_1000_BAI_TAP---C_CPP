1bf9416ae67542b671d031fdea498f4fd90fa9d5
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:03:58 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai948/main.cpp
@@ -0,0 +1,8 @@
+/*
+
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một cầu thủ. Biết rằng một cầu thủ gồm những thành phần sau:
+- Mã cầu thủ: chuỗi tối đa 10 kí tự.
+- Tên cầu thủ: chuỗi tối đa 30 kí tự.
+- Ngày sinh: kiểu dữ liệu ngày.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
