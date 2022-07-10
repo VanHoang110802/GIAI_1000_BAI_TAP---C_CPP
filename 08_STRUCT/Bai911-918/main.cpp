c285f03fa83c46d37115c64f13b5cbf2e8510e12
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:46:00 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai911-918/main.cpp
@@ -0,0 +1,11 @@
+/*
+Đường tròn trong mặt phẳng Oxy
+Khai báo kiểu dữ liệu để biểu diễn đường tròn.
+Nhập đường tròn.
+Xuất đường tròn theo định dạng ((x,y),r).
+Tính chu vi đường tròn.
+Tính diện tích đường tròn.
+Xét vị trí tương đối giữa hai đường tròn (không cắt nhau, tiếp xúc, cắt nhau).
+Kiểm tra một toạ độ điểm có nằm trong đường tròn hay không.
+Cho hai đường tròn. Tính diện tích phần mặt phẳng bị phủ bởi hai đường tròn đó.
+*/
