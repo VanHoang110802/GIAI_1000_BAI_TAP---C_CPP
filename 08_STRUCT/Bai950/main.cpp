c2704a80089e4698c3aae10a57ec4d1b5f740eaf
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:05:34 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai950/main.cpp
@@ -0,0 +1,8 @@
+/*
+
+Khai bao kiểu dữ liệu để biểu diễn thông tin của một nhân viên. Biết rằng một nhân viên gồm những thành phần như sau:
+- Mã nhân viên: chuỗi tối đa 5 kí tự.
+- Tên nhân viên: chuỗi tối đa 30 kí tự.
+- Lương nhân viên: kiểu số thực.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
