2c254c01edf994dd9fe7a3608cad3c9ccdb5e312
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:58:28 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai945/main.cpp
@@ -0,0 +1,7 @@
+/*
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một hộp sữa. Biết rằng một hộp sữa gồm những thành phần như sau:
+- Nhãn hiệu: chuỗi tối đa 20 ký tự.
+- Trọng lượng kiểu số thực.
+- Hạn sử dụng: kiểu dữ liệu ngày.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
