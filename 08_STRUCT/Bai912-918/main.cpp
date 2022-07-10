6bc7ff6df32ab273bdd2e34b17d3f8d09257d662
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:47:04 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai912-918/main.cpp
@@ -0,0 +1,11 @@
+/*
+Hình cầu trong không gian Oxyz
+Khai báo kiểu dữ liệu để biểu diễn hình câu trong không gian Oxyz.
+Nhập hình cầu.
+Xuất đường cầu theo định dạng ((x,y,z), r).
+Tính diện tích xung quanh hình cầu.
+Tính thể tích hình cầu.
+Xét vị trí tương đối giữa hai hình câu (không cát nhau, tiếp xúc, cắt nhau).
+Kiểm tra một toạ độ điểm có nằm bên trong hình cầu hay không.
+*/
+
