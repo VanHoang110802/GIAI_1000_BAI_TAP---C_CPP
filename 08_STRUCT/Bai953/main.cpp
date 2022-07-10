3aedecdc60d58452f7d9692d084f94cd0d85b18e
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:10:24 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai953/main.cpp
@@ -0,0 +1,10 @@
+/*
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một đại lý(DAILY). Biết rằng một đại lý gồm những thành phần như sau:
+- Mã đại lý: chuỗi tối đa 5 ký tự.
+- Tên đại lý: chuỗi tối đa 30 ký tự.
+- Điện thoại : kiểu số nguyên 4 byte.
+- Ngày tiếp nhận: kiểu dữ liệu ngày.
+- Địa chỉ: chuỗi tối đa 50 ký tự.
+- E-Mail: chuỗi tối đa 50 ký tự.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
