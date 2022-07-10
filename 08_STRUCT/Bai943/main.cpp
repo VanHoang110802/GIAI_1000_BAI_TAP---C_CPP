7f81be59650ff954329446dde68da9deaebc7994
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:57:22 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai943/main.cpp
@@ -0,0 +1,9 @@
+/*
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một tỉnh.
+Biết rằng một tỉnh gồm những thành phần như sau:
+- Mã tỉnh: kiểu số nguyên 2 byte.
+- Tên tỉnh: chuỗi tối đa 30 ký tự.
+- Dân số: kiểu số nguyên 4 byte.
+- Diện tích: kiểu số thực.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
