0cdaa5a4e33903e4b81ef978b8e8d059b33a07c2
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:55:00 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai931-942/main.cpp
@@ -0,0 +1,17 @@
+/*
+Ngày
+Khai báo kiểu dữ liệu để biểu diễn ngày.
+Nhập ngày.
+Xuất ngày theo định dạng (ngày/tháng/năm).
+Kiểm tra năm nhuận.
+Tính số thứ tự ngày trong năm.
+Tính số thứ tự ngày kể từ ngày 1/1/1.
+Tìm ngày khi biết năm và số thứ tự của ngày trong năm.
+Tìm ngày khi biết số thứ tự ngày kể từ ngày 1/1/1.
+Tìm ngày kế tiếp.
+Tìm ngày hôm qua.
+Tìm ngày kế đó k ngày.
+Tìm ngày trước đó k ngày.
+Khoảng cách giữa hai ngày.
+So sánh hai ngày.
+*/
