9ccbc64aa4685e305b158a1235f1285a860c58a4
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 17:09:34 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai952/main.cpp
@@ -0,0 +1,10 @@
+/*
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một sổ tiết kiệm. Biết rằng một sổ tiết kiệm gồm những thành phần như sau:
+- Mã số: chuỗi tối đa 5 ký tự.
+- Loại tiết kiệm: chuỗi tối đa 10 ký tự.
+- Họ tên khách hàng: chuỗi tối đa 30 ký tự.
+- Chứng minh nhân dân: kiểu số nguyên 4 byte.
+- Ngày mở sổ: kiểu dữ liệu ngày.
+- Số tiền gửi: kiểu số thực.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
