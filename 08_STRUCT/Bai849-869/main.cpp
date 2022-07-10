25b0494a0abde50c24228001b4ae2e365d4002f7
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:38:05 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai849-869/main.cpp
@@ -0,0 +1,18 @@
+/*
+Rút gọn phân số.
+Tính tổng hai phân số.
+Tính hiệu hai phân số.
+Tính tích hai phân số.
+Tính thương hai phân số.
+Kiểm tra phân số tối giản
+Qui đồng hai phân số.
+Kiểm tra phân số dương.
+Kiểm tra phân số âm.
+So sánh hai phân số (Hàm sẽ trả về một trong 3 giá trị 0, -1, 1).
+Định nghĩa toán tử cộng (operator +) cho hai phân số.
+Định nghĩa toán tử hiệu (operator -) cho hai phân số.
+Định nghĩa toán tử tích (operator *) cho hai phân số.
+Định nghĩa toán tử thương (operator /) cho hai phân số.
+Định nghĩa toán tử tăng một (operator ++) cho hai phân số.
+Định nghĩa toán tử giảm một (operator --) cho hai phân số.
+*/
