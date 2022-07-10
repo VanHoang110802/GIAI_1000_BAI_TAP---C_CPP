c294402328e83186769f79c25e087030a92d0a99
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:59:56 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai946/main.cpp
@@ -0,0 +1,7 @@
+/*
+Hay khai báo kiểu dữ liệu để biểu diễn thông tin của một mặt hàng. Biết rằng một mặt hàng gồm những thành phần như sau:
+- Tên mặt hàng: chuỗi tối đa 20 ký tự.
+- Đơn giản: kiểu số nguyên 4 byte.
+- Số lượng tồn: kiểu số nguyên 4 byte.
+Sau đó viết hàm nhập và hàm xuất cho kiểu dữ liệu này.
+*/
