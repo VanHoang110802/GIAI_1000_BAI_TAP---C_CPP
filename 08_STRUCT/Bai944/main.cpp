f00c080b17f43f43a9e9751e3e28393f99c0deb8
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:59:14 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai944/main.cpp
@@ -0,0 +1,8 @@
+/*
+Hãy khai báo kiểu dữ liệu để biểu diễn thông tin của một vé xem phim. Biết rằng một vé xem phim gồm những thành phần như sau:
+- Tên phim: chuỗi tối đa 20 ký tự.
+- Giá tiền: kiểu số nguyên 4 byte.
+- Xuất chiếu: kiểu thời gian.
+- Ngày xem: kiểu dữ liệu ngày.
+Sau đó viết hàm nhập và xuất cho kiểu dữ liệu này.
+*/
