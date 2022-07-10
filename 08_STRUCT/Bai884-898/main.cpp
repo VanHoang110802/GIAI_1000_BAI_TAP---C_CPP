193258d47e65abd04edced944f18ae6194b05cd3
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-10 16:42:29 +0700

    Create main.cpp

---
+++ 08_STRUCT/Bai884-898/main.cpp
@@ -0,0 +1,18 @@
+/*
+Điểm trong mặt phẳng Oxy
+Khai báo kiểu dữ liệu.
+Nhập toạ độ điểm trong mặt phẳng.
+Xuất toạ độ điểm theo định dạng (x,y).
+Tính khoảng cách giữa hai điểm.
+Tính khoảng cách giữa hai điểm theo phương Ox.
+Tính khoảng cách giữa hai điểm theo phương Oy.
+Tìm toạ độ điểm đối xứng qua gốc toạ độ.
+Tìm toạ độ điểm đối xứng qua trục hoành.
+Tìm toạ độ điểm đối xứng qua trục tung.
+Tìm toạ độ điểm đối xứng qua đường phân giác thứ nhất (y = x).
+Tìm toạ độ điểm đối xứng qua đường phân giác thứ hai (y = -x).
+Kiểm tra điểm có thuộc phần tư thứ I không?.
+Kiểm tra điểm có thuộc phần tư thứ II không?.
+Kiểm tra điểm có thuộc phần tư thứ III không?.
+Kiểm tra điểm có thuộc phần tư thứ IV không?.
+*/
