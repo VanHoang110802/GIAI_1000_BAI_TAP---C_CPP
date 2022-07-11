18f9c683bd4fe160ebf04be3df6911cf290417fa
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-11 08:19:23 +0700

    Update DaThuc.cpp

--- 08_STRUCT/Bai839-848/DaThuc.cpp
+++ 08_STRUCT/Bai839-848/DaThuc.cpp
@@ -35,6 +35,16 @@ void XuatDaThuc(DATHUC* dt)
 		cout << dt->heso[0];
 	}
 }
+void Xuat(string name, Poly A, int qd)
+{
+	cout << name << " ";
+	for (int i = 0; i < qd - 1; ++i)
+	{
+		cout << A[i] << "x^" << i << " + ";
+	}
+	cout << A[qd - 1] << "x^" << qd - 1;
+	cout << "\n";
+}
 void* TinhTong2DT(DATHUC *X, DATHUC *Y)
 {
 	int Max = X->sobac > Y->sobac ? X->sobac : Y->sobac;
@@ -116,6 +126,97 @@ void* TinhTich2DT(DATHUC* A, DATHUC* B)
 	delete Kq;
 	return NULL;
 }
+void* TinhThuong2DT(DATHUC* dt1, DATHUC* dt2)
+{
+	Poly HeSoDaThuc1, HeSoDaThuc2, d, q, r;        // vectors - N / D == q && N % D == r
+	int SoBacdathuc1, SoBacdathuc2, dd, dq, dr; // degrees of vectors
+	int i;                  // loop counter
+	
+	SoBacdathuc1 = dt1->sobac;
+	SoBacdathuc2 = dt2->sobac;
+
+	dq = SoBacdathuc1 - SoBacdathuc2;	// dq = 3
+	dr = SoBacdathuc1 - SoBacdathuc2;
+
+	if (SoBacdathuc2 < 1 || SoBacdathuc1 < 1)
+	{
+		cerr << "He so bac cua da thuc khong duoc am, hay kiem tra lai!\n";
+		return 0;
+	}
+
+	HeSoDaThuc1 = new float[SoBacdathuc1 + 1];
+
+	for (i = 0; i <= SoBacdathuc1; i++)
+	{
+		HeSoDaThuc1[i] = dt1->heso[i];
+	}
+
+	HeSoDaThuc2 = new float[SoBacdathuc1 + 1];
+
+	for (i = 0; i <= SoBacdathuc2; i++)
+	{
+		HeSoDaThuc2[i] = dt2->heso[i];
+	}
+
+	d = new float[SoBacdathuc1 + 1];
+	q = new float[dq + 1];
+	r = new float[dr + 1];
+	cout << "\n";
+	// khởi tạo cho các phần tử trong mảng = 0 hết
+	fill(d, d + SoBacdathuc1 + 1, 0);
+	fill(q, q + dq + 1, 0);
+	fill(r, r + dr + 1, 0);
+	if (SoBacdathuc1 >= SoBacdathuc2)
+	{
+		while (SoBacdathuc1 >= SoBacdathuc2)
+		{
+			// khởi tạo lại 1 lần nữa để tránh lỗi tràn số
+			fill(d, d + 1, 0);
+			fill(q, q + 1, 0);
+
+			for (i = 0; i <= SoBacdathuc2; i++)
+			{
+				d[i + (SoBacdathuc1 - SoBacdathuc2)] = HeSoDaThuc2[i];
+			}
+			
+			dd = SoBacdathuc1;
+
+
+			q[SoBacdathuc1 - SoBacdathuc2] = HeSoDaThuc1[SoBacdathuc1] / d[dd];
+
+			
+			// d = d * q[dN-dD]
+			for (i = 0; i < dq + 1; i++)
+			{
+				d[i] = d[i] * q[SoBacdathuc1 - SoBacdathuc2];
+			}
+			
+			// N = N - d
+			for (i = 0; i < SoBacdathuc1 + 1; i++)
+			{
+				HeSoDaThuc1[i] = HeSoDaThuc1[i] - d[i];
+			}
+
+			SoBacdathuc1--;
+
+		}
+	}
+
+	// r = N 
+	for (i = 0; i <= SoBacdathuc1; i++)
+	{
+		r[i] = HeSoDaThuc1[i];
+	}
+
+	Xuat("Thuong cua 2 da thuc: ", q, dq + 1);
+	Xuat("Du cua 2 da thuc: ", r, dr + 1);
+
+	delete[] HeSoDaThuc1;
+	delete[] HeSoDaThuc2;
+	delete[] d;
+	delete[] q;
+	delete[] r;
+}
 /*
 Nếu mà muốn tính đạo hàm cấp 1 cho đa thức, thì đổi DATHUC* -> void* nhé 
 
@@ -132,7 +233,6 @@ DATHUC* TinhDaoHamCap1(DATHUC *dt)
 	{
 		ketqua->heso[i - 1] = dt->heso[i] * i;
 	}
-	cout << "\n\n";
 	//XuatDaThuc(ketqua);
 	//delete ketqua;
 	return ketqua;
