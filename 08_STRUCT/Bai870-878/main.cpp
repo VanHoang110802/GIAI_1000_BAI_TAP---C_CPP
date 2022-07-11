d3a0da7547e3177281879b906ec8ad4daca9bad3
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-11 11:31:14 +0700

    Update main.cpp

--- 08_STRUCT/Bai870-878/main.cpp
+++ 08_STRUCT/Bai870-878/main.cpp
@@ -8,5 +8,47 @@ Tính hiệu hai hỗn số.
 Tính tích hai hỗn số.
 Tính thương hai hỗn số.
 Kiểm tra hỗn số tối giản.
-Qui đồng hay hỗn số.
+Qui đồng hai hỗn số.
 */
+
+#include "HonSo.h"
+int main()
+{
+	HONSO hs1, hs2, ketqua;
+	NhapHonSo(hs1);
+	NhapHonSo(hs2);
+	cout << "\n";
+	XuatHonSo(hs1);
+	cout << "\n";
+	XuatHonSo(hs2);
+
+	ketqua = TinhTong(hs1, hs2);
+	cout << "\nTong cua 2 hon so la: ";
+	XuatHonSo(ketqua);
+	ketqua = TinhHieu(hs1, hs2);
+	cout << "\nHieu cua 2 hon so la: ";
+	XuatHonSo(ketqua);
+	ketqua = TinhTich(hs1, hs2);
+	cout << "\nTich cua 2 hon so la: ";
+	XuatHonSo(ketqua);
+	ketqua = TinhThuong(hs1, hs2);
+	cout << "\nThuong cua 2 hon so la: ";
+	XuatHonSo(ketqua);
+
+	if (KiemTraToiGian(hs1))
+	{
+		cout << "\nHon so (hs1) goc, da toi gian!";
+	}
+	else cout << "\nHon so (hs1) goc, chua toi gian!";
+	if (KiemTraToiGian(hs2))
+	{
+		cout << "\nHon so (hs2) goc, da toi gian!";
+	}
+	else cout << "\nHon so (hs2) goc, chua toi gian!";
+	QuyDong2HS(hs1, hs2);
+	cout << "\nHon so (hs1) sau khi qui dong: ";
+	XuatHonSo(hs1);
+	cout << "\nHon so (hs2) sau khi qui dong: ";
+	XuatHonSo(hs2);
+	return 0;
+}
