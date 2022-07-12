13d8b857e52ea1037aac822f32f977bdf314b521
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-12 12:37:44 +0700

    Create DiemTrongKG.h

---
+++ 08_STRUCT/Bai911-918/DiemTrongKG.h
@@ -0,0 +1,17 @@
+#ifndef _khonggianOxy_
+#define _khonggianOxy_
+
+#include <iostream>
+#include <cmath>
+#define Pi 3.14159265
+using namespace std;
+
+struct Diem
+{
+	float x, y;
+};
+typedef struct Diem diem;
+
+void NhapDiem(diem&);
+
+#endif
