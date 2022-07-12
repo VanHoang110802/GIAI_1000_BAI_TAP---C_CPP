8382cf0827ef02aa12f281099210d71e6ae407d5
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-12 14:21:07 +0700

    Update main.cpp

--- 08_STRUCT/Bai919-930/main.cpp
+++ 08_STRUCT/Bai919-930/main.cpp
@@ -13,3 +13,66 @@ Tính tổng khoảng cách từ điểm P(x, y) tới 3 đỉnh của tam giác
 Kiểm tra một toạ độ điểm có nằm trong tam giác hay không.
 Hãy cho biết dạng của tam giác (đều, vuông, vuông cân, cân, thường).
 */
+
+#include "HinhTamGiac.h"
+
+int main()
+{
+	tamgiac tg;
+
+	NhapHinhTamGiac(tg);
+
+	XuatHinhTamGiac(tg);
+
+	float ChuVi = TinhChuViTamGiac(tg);
+	float DienTich = TinhDienTichTamGiac(tg);
+
+	diem G = TimToaDoTrongTamTamGiac(tg);
+
+	cout << "\nChu vi tam giac la: "<< ChuVi;
+	cout << "\nDien tich tam giac la: "<< DienTich;
+	cout << "\nToa do trong tam tam giac la <" << G.x << ", " << G.y << ">";
+
+	diem I = TimDinhCoHoanhDoLonNhat(tg);
+	cout << "\nDinh co hoanh do lon nhat la: ";
+	XuatDiem(I);
+
+	diem J = TimDinhCoTungDoNhoNhat(tg);
+	printf("\nDinh co tung do nho nhat la: ");
+	XuatDiem(J);
+
+	float KhoangCach = TinhKhoangCachTuPToi3Dinh(tg, G);
+	cout << "\nkhoang cach la: " << KhoangCach;
+
+	int  dang = DangTamGiac(tg);
+
+	if (dang == 1)
+	{
+		cout << "\nTam giac thuong";
+	}
+	else
+	{
+		if (dang == 2)
+		{
+			cout << "\nTam giac vuong ";
+		}
+		if (dang == 3)
+		{
+			cout << "\nTam giac can";
+		}
+		else
+		{
+			cout << "\nTam giac thuong";
+		}
+	}
+
+	if (KiemTra1DiemCoNamTrongTamGiac(tg, G))
+	{
+		cout << "\nCo diem nam trong tam giac";
+	}
+	else
+	{
+		cout << "\nKhong co diem nam trong tam giac";
+	}
+	return 0;
+}
