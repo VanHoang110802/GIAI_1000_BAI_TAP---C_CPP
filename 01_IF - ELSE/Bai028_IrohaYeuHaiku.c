b1156dc8088634efca7e0ba66e9d03923994615c
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-14 12:47:05 +0700

    Update and rename Bai028.c to Bai028_IrohaYeuHaiku.c

--- 01_IF - ELSE/Bai028_IrohaYeuHaiku.c
+++ 01_IF - ELSE/Bai028_IrohaYeuHaiku.c
@@ -1,5 +1,6 @@
 Iroha yêu Haiku. Haiku là một dạng thơ ngắn của Nhật Bản. Một bài Haiku bao gồm ba câu với số ký tự lần lượt là 5, 7 và 5.
-Để tạo ra một bài thơ Haiku. Iroha đã nghĩ ra ba câu khác nhau. Mỗi câu có độ dài tương ứng là A, B và C. Xác định xem cô ấy có thể tạo một bài Haiku bằng cách sử dụng 3 câu trên mỗi câu 1 lần hay không? (1 <= A,B,C <= 10)
+Để tạo ra một bài thơ Haiku. Iroha đã nghĩ ra ba câu khác nhau. Mỗi câu có độ dài tương ứng là A, B và C. Xác định xem cô ấy có thể tạo một bài Haiku bằng cách sử dụng 3
+câu trên mỗi câu 1 lần hay không? (1 <= A,B,C <= 10)
 input: 5 5 7	->output: YES
 input: 7 7 5	->output: NO
 
