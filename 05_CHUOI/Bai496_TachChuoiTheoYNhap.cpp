8efe60e2da4015c1ae95b51a124c4300b5131bb9
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-14 17:06:15 +0700

    Update and rename Bai496.cpp to Bai496_TachChuoiTheoYNhap.cpp

---
+++ 05_CHUOI/Bai496_TachChuoiTheoYNhap.cpp
@@ -0,0 +1,2 @@
+Nhập một chuỗi bất kỳ, sau đó hỏi người dùng cần tách bắt đầu từ đâu trong chuỗi trở về sau.
+Ví dụ: Nhập chuỗi S1:”Trường Cao Dang FPT Polytechnic”. Người nhập muốn tách bắt đầu từ chữ “FPT” thì sẽ xuất ra chuỗi “FPT Polytechnic” ra màn hình
