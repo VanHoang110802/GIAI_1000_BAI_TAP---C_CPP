b19a0277e7a2e10fdfaaaa77ee352c0cc695c660
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-14 12:50:36 +0700

    Update and rename Bai023.cpp to Bai023_PokemonTradingCard.cpp

--- 01_IF - ELSE/Bai023_PokemonTradingCard.cpp
+++ 01_IF - ELSE/Bai023_PokemonTradingCard.cpp
@@ -1,5 +1,7 @@
-Long và Linh là một đôi bạn thân, vào những lúc rảnh rỗi, họ thường bày ra những trò để chơi cùng nhau. Trong đó, trò chơi mà họ ưa thích nhất đó là Pokémon Trading Card Game (Pokémon TCG).
-Về cơ bản, luật của trò chơi đó là mỗi người chơi sẽ có một lá bài, và lá bài nào có "chỉ số sức mạnh" lớn hơn sẽ thắng. Biết rằng lá bài của Long có chỉ số sức mạnh khác lá bài của Linh.
+Long và Linh là một đôi bạn thân, vào những lúc rảnh rỗi, họ thường bày ra những trò để chơi cùng nhau. Trong đó, trò chơi mà họ ưa thích nhất đó là Pokémon Trading Card
+Game (Pokémon TCG).
+Về cơ bản, luật của trò chơi đó là mỗi người chơi sẽ có một lá bài, và lá bài nào có "chỉ số sức mạnh" lớn hơn sẽ thắng. Biết rằng lá bài của Long có chỉ số sức mạnh khác
+lá bài của Linh.
 Bạn được cho biết "chỉ số sức mạnh" của lá bài mỗi người chơi có, hãy cho biết ai sẽ là người thắng cuộc.
 Ở lần chơi trước, Long hí hửng vì có được 1 con Vulpix, nhưng Linh lại có được tận 1 con Charizard, vì thế nên Long thua.
 Nếu Long là người thắng cuộc, in ra 1. Còn nếu Linh là người thắng cuộc, in ra 0.
