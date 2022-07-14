b19023506f8146d9d971e7afbbaa45b6d0e7a275
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-14 16:59:46 +0700

    Update and rename Bai454.cpp to Bai454_STRTOK.cpp

--- 05_CHUOI/Bai454_STRTOK.cpp
+++ 05_CHUOI/Bai454_STRTOK.cpp
@@ -1,11 +1,21 @@
 Viết lại hàm cắt chuỗi (strtok): 
-chúng ta cần hàm tìm kiếm 1 cái chuỗi trong 1 chuỗi cho trước, xem vị trí đầu tiên nó nằm ở vị trí nào. Rồi cần 1 hàm cắt chuỗi đem chuỗi vừa tìm được vừa cắt được đó đem ra ngoài.
+chúng ta cần hàm tìm kiếm 1 cái chuỗi trong 1 chuỗi cho trước, xem vị trí đầu tiên nó nằm ở vị trí nào. Rồi cần 1 hàm cắt chuỗi đem chuỗi vừa tìm được vừa cắt được đó 
+đem ra ngoài.
 (19/02/2022 - Đã hoàn thành ^^ - Bài này khó vcc, tốn kha khá time @@ ).
 
 /*
-Đê làm được hàm này, trước hết ta sẽ có hàm cắt chuỗi con trong đoạn từ [a,b] trong chuỗi s đã cho trước, nhưng bên C này ta không có hàm này, chỉ có bên C++ mới có(substr) vì vậy ta sẽ phải tự đi xây dựng hàm đó thôi.
-Rồi ta sẽ dùng hàm strstr để xác định vị trí bắt đầu của chuỗi con hoặc nếu tự viết lại thì ta có 1 biến x, biến x này tượng trưng cho vị trí bắt đầu của cái chuỗi s ban đầu, rồi sau đó duyệt từ vị trí x đến độ dài cuối cùng của chuỗi, nếu mà phần tử đầu của chuỗi tìm kiếm mà giống với 1 phần tử đầu của 1 chuỗi đã cho trước (chuỗi s) thì lập tức ta có 1 biến phụ lưu lại vị trí đó (vì vị trí sẽ có thay đổi nên ta phải có biến phụ lưu trữ lại), cập nhật cho check = 1, rồi đi duyệt lại cái chuỗi cần tìm kiếm, nếu trong lúc duyệt phần tử cần tìm kiếm đó mà có 1 vị trí nào đó trong chuỗi mà khác với vị trí chứa phần tử của chuỗi s ban đầu, thì lập tức cho check = 0 rồi thoát khỏi vòng lặp luôn, 1 vị trí nào đó mà khác nhau rồi thì ta không cần đi xét tiếp nữa làm gì, chỉ cần đi xét tiếp nếu chúng giống nhau mà thôi. Nếu đi hết tất cả các vị trí trong vòng lặp rồi mà thỏa hết các vị trí với nhau thì check = 1 và return lại biến phụ đã lưu lại vị trí đó.
-Tiếp theo, ta sẽ cho biến x = 0, vị trí = -1 (thực chất chuỗi chính là 1 mảng 1 chiều các kí tự, mà mảng thì vị trí bắt đầu từ 0, làm gì có vị trí nào bắt đầu từ -1 đâu). Có vòng lặp vô tận, lúc này x = vị trí + 1 (như đã nói ở trên, mảng bắt đầu vị trí 0), rồi ta cập nhật lại vị trí của chuỗi chính là vị trí bắt đầu của chuỗi con đã tìm được ở bên trên kia, nếu vị trí = -1 thì ta sẽ đi duyệt chuỗi s rồi in hết các kí tự có trong chuỗi này. Nếu ngược lại, vị trí tìm thấy mà khác -1 thì lúc này ta chỉ việc cắt chuỗi tại vị trí đã tìm được ra rồi đi in ra thôi.
+Đê làm được hàm này, trước hết ta sẽ có hàm cắt chuỗi con trong đoạn từ [a,b] trong chuỗi s đã cho trước, nhưng bên C này ta không có hàm này, chỉ có bên C++ mới có
+(substr) vì vậy ta sẽ phải tự đi xây dựng hàm đó thôi.
+Rồi ta sẽ dùng hàm strstr để xác định vị trí bắt đầu của chuỗi con hoặc nếu tự viết lại thì ta có 1 biến x, biến x này tượng trưng cho vị trí bắt đầu của cái chuỗi s
+ban đầu, rồi sau đó duyệt từ vị trí x đến độ dài cuối cùng của chuỗi, nếu mà phần tử đầu của chuỗi tìm kiếm mà giống với 1 phần tử đầu của 1 chuỗi đã cho trước 
+(chuỗi s) thì lập tức ta có 1 biến phụ lưu lại vị trí đó (vì vị trí sẽ có thay đổi nên ta phải có biến phụ lưu trữ lại), cập nhật cho check = 1, rồi đi duyệt lại cái
+chuỗi cần tìm kiếm, nếu trong lúc duyệt phần tử cần tìm kiếm đó mà có 1 vị trí nào đó trong chuỗi mà khác với vị trí chứa phần tử của chuỗi s ban đầu, thì lập tức cho
+check = 0 rồi thoát khỏi vòng lặp luôn, 1 vị trí nào đó mà khác nhau rồi thì ta không cần đi xét tiếp nữa làm gì, chỉ cần đi xét tiếp nếu chúng giống nhau mà thôi. Nếu
+đi hết tất cả các vị trí trong vòng lặp rồi mà thỏa hết các vị trí với nhau thì check = 1 và return lại biến phụ đã lưu lại vị trí đó.
+Tiếp theo, ta sẽ cho biến x = 0, vị trí = -1 (thực chất chuỗi chính là 1 mảng 1 chiều các kí tự, mà mảng thì vị trí bắt đầu từ 0, làm gì có vị trí nào bắt đầu từ -1 
+đâu). Có vòng lặp vô tận, lúc này x = vị trí + 1 (như đã nói ở trên, mảng bắt đầu vị trí 0), rồi ta cập nhật lại vị trí của chuỗi chính là vị trí bắt đầu của chuỗi con
+đã tìm được ở bên trên kia, nếu vị trí = -1 thì ta sẽ đi duyệt chuỗi s rồi in hết các kí tự có trong chuỗi này. Nếu ngược lại, vị trí tìm thấy mà khác -1 thì lúc này 
+ta chỉ việc cắt chuỗi tại vị trí đã tìm được ra rồi đi in ra thôi.
 */
  /*
  #define _CRT_SECURE_NO_WARNINGS
