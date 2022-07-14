e58ad4bd2bbe39e95715cf43ce7173315db14dc8
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2022-07-14 17:18:21 +0700

    Update and rename Bai476.cpp to Bai476_DemKhoangTrang.cpp

--- 05_CHUOI/Bai476_DemKhoangTrang.cpp
+++ 05_CHUOI/Bai476_DemKhoangTrang.cpp
@@ -1,10 +1,13 @@
-Cho một chuỗi ký tự chỉ gồm các ký tự chữ cái latinh, chữ số và dấu cách. Một khoảng trống là một đoạn liên tiếp các ký tự trống (dấu cách) trong chuỗi. Hãy đếm số lượng khoảng trống trong chuỗi.
+Cho một chuỗi ký tự chỉ gồm các ký tự chữ cái latinh, chữ số và dấu cách. Một khoảng trống là một đoạn liên tiếp các ký tự trống (dấu cách) trong chuỗi. Hãy đếm số
+lượng khoảng trống trong chuỗi.
 Dòng đầu chứa số nguyên dương T là số chuỗi; dòng tiếp theo, mỗi dòng chứa một chuỗi có độ dài không quá 1000 ký tự.
 input: 2		-> output: 
 abc   xyz ab			 2
  abc def ghi 			3	  
 /*
-Ta sẽ đi xác định độ dài của chuỗi, rồi sau đó đi duyệt từ cuối về đầu chuỗi, dùng hàm isspace (kiểm tra xem ký tự được truyền vào có phải là khoảng trắng hay không) trong thư viện #include<ctype.h> hỗ trợ để kiểm tra phần tử nào là khoảng trắng, nếu là khoảng trắng thì đếm tăng lên. Hoặc đi kiểm tra phần tử hiện tại là khoảng trắng và trước đó là 1 ký tự khác khoảng trắng thì đếm cũng được tăng lên, cứ thế cho đến hết chuỗi thì ngừng.
+Ta sẽ đi xác định độ dài của chuỗi, rồi sau đó đi duyệt từ cuối về đầu chuỗi, dùng hàm isspace (kiểm tra xem ký tự được truyền vào có phải là khoảng trắng hay không)
+trong thư viện #include<ctype.h> hỗ trợ để kiểm tra phần tử nào là khoảng trắng, nếu là khoảng trắng thì đếm tăng lên. Hoặc đi kiểm tra phần tử hiện tại là khoảng 
+trắng và trước đó là 1 ký tự khác khoảng trắng thì đếm cũng được tăng lên, cứ thế cho đến hết chuỗi thì ngừng.
 
 Lưu ý: NHẤN ENTER, VẪN BỊ COI LÀ 1 KHOẢNG TRẮNG, vậy nên tránh lỗi này, ta dùng thêm strtok cắt "\n" này đi là ổn.
 (Nội dung tham khảo thư viện #include<ctype.h> ở bên dưới nè).
